7cfd923f3c9447636e307d6d5177a4ee6040492e
A. R. Arif <ararif@gmail.com>
2022-10-09 21:26:21 +0600

    fixed error reporting

--- public/index.php
+++ public/index.php
@@ -1,6 +1,12 @@
 <?php
 require_once('../backend/route.php');
-if(getenv('DEBUG') != false){
+if (getenv('DEBUG') == 'true') {
+    ini_set('display_errors', '1');
+    ini_set('display_startup_errors', '1');
+    error_reporting(E_ALL);
+} else {
+    ini_set('display_errors', '0');
+    ini_set('display_startup_errors', '0');
     error_reporting(0);
 }
 
