296ff7c0bd13f8461b685d83217af148db5261f7
A. R. Arif <ararif@gmail.com>
2022-10-09 22:17:29 +0600

    app name changed

--- backend/pages/home.php
+++ backend/pages/home.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Telnyx App</title>
+    <title>SMS App</title>
 </head>
 <body>
     <div id="root"></div>
